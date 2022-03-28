6f2fd599b4838816af547b3d25878b26eb66c7be
atsuunimog <atsudominicsamuel@gmail.com>
2022-03-28 14:04:38 +0100

    added banner to blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,20 +1,29 @@
 <x-guest-layout>
-    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <div>
-            <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-              All Blog Post
-            </p>
+  <div class=" h-80" style='background-image:url({{ asset('storage/GMAP-Banner3.jpg') }}); background-size:cover;'>
+    <div class="container mx-auto px-4 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div class='grid grid-cols-2'>
+          <div class=''>
+            <!--no content-->
           </div>
-          <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-           Blog
-          </h2>
-          <p class="text-base text-gray-700 md:text-lg">
-              Read blogs from over 1000+ schools
-          </p>
-        </div>
+          <div class='flex sm:items-center h-80'>
+            <!--info get me a school-->
+            <div class='py-4'>
+              <h1 class='font-bold text-5xl text-white mb-1'>
+                Blog Spot
+              </h1>
+              <p class="text-2xl text-white">
+                Read blogs from over <strong>1000+</strong> active schools on Trenova consulting
+              </p>
+            </div>
+            <!--info get me a school-->
+          </div>
+      </div>
+    </div>
+    </div>
+    
 
 
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div class="grid gap-5 row-gap-5 mb-8 lg:grid-cols-4 sm:grid-cols-2">
 
           @foreach ($blog_data as $data)
@@ -24,7 +33,7 @@
               <div class="flex-grow border border-t-0 rounded-b">
                 <div class="p-5">
 
-                  <div class='flex'>
+                  <div class='flex text-sm mb-2'>
                     <div class='w-full'>
                       <p class="font-normal m-0 p-0 text-gray-600 uppercase">
                         {{Carbon\Carbon::parse($data->created_at)->format('D, d')}}
@@ -32,9 +41,8 @@
                     </div>
                     <div class="text-center w-full">
                       <!--like icon-->
-                     <i class="icon ion-eye text-gray-500"></i>
+                    <p class="font-normal m-0 p-0 text-gray-600 uppercase">VIEWS <strong class='text-blue-600'>{{$data->views}}</strong></p>
                       <!--like icon-->
-                      {{$data->views}}
                    </div>
                 </div>
 
@@ -42,7 +50,7 @@
                   <p class="font-normal truncate text-gray-500">
                    {{$data->blog_content}}
                   </p>
-                  <p><span class='text-xs font-bold block py-1 text-blue-500'>AUTHOR</span>
+                  <p><span class='text-xs font-bold block py-1 text-blue-600'>AUTHOR</span>
                   <span class='mb-2 leading-5 text-black'>{{$data->school_name}}</span></p>
                 
                 </div>
